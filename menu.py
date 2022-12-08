dadffd48e60b10d55db6dae7f24c1755e99c555d
edwinkullu <edwinkullu94@gmail.com>
2022-12-08 15:14:18 +0530

    Updated data

--- menu.py
+++ menu.py
@@ -1,9 +1,11 @@
-num1=input("Enter number1")
-num2=input("Enter number2")
+#num1=input("Enter number1")
+#num2=input("Enter number2")
 
-add= num1 + num2
-sub=num1-num2
-mul=num1 * num2
-div=num1 % num2
+#add= num1 + num2
+#sub=num1-num2
+#mul=num1 * num2
+#div=num1 % num2
 
-print(add)
+#print(add)
+
+print("Hellow world")
