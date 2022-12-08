215fdccaf20dc24dd85fb65354ca613d2b6b7053
edwinkullu <edwinkullu94@gmail.com>
2022-12-08 16:04:31 +0530

    Updated data

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python
+
+COPY menu.py \C:\Users\edwin\Desktop\labexam\
+EXPOSE  
--- menu.py
+++ menu.py
@@ -1,11 +1,14 @@
-#num1=input("Enter number1")
-#num2=input("Enter number2")
+num1=5
+num2=2
 
-#add= num1 + num2
-#sub=num1-num2
-#mul=num1 * num2
-#div=num1 % num2
+add= num1+num2
+sub=num1-num2
+mul=num1 * num2
+div=num1 % num2
 
-#print(add)
+print(add)
+print(sub)
+print(mul)
+print(div)
 
 print("Hellow world")
